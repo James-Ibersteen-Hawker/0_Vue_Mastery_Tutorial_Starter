b85060f7c3c4dc4a7d624866d153c9e6996cdcaa
James Hawker <526rserbinenko@frhsd.com>
2025-11-25 23:24:00 +0000

    Attribute Binding

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
       product: "Socks",
       description: "These socks are woolly and wonderful!",
       image: "./assets/images/socks_green.jpg",
+      url: "https://google.com",
     };
   },
 });
