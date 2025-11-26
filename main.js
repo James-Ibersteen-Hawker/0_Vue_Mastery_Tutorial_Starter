086fc5a54c22ed690e382c3720ff118855c6cd58
James Hawker <526rserbinenko@frhsd.com>
2025-11-26 00:36:57 +0000

    Event Handling

--- main.js
+++ main.js
@@ -12,13 +12,27 @@ const app = Vue.createApp({
         {
           id: 2234,
           color: "green",
+          image: "./assets/images/socks_green.jpg",
         },
         {
           id: 2235,
           color: "blue",
+          image: "./assets/images/socks_blue.jpg",
         },
       ],
       sizes: ["sm", "reg", "lg", "xxl"],
+      cart: 0,
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart++;
+    },
+    removeFromCart() {
+      this.cart--;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
